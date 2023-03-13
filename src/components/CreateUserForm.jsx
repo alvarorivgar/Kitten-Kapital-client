9831606b866eddf83cf53281163941dcba6c5ddd
Alvaro <alvarorivgar@gmail.com>
2023-03-13 17:36:14 +0100

    added redirect after creating user to create a new account

--- src/components/CreateUserForm.jsx
+++ src/components/CreateUserForm.jsx
@@ -6,7 +6,7 @@ import { Navigate } from "react-router-dom";
 
 function CreateUserForm() {
   const navigate = useNavigate();
-  const { isLoggedIn, isAdmin, loggedUser } = useContext(AuthContext)
+  const { isLoggedIn, isAdmin } = useContext(AuthContext);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [idNumber, setIdNumber] = useState("");
@@ -36,32 +36,24 @@ function CreateUserForm() {
       password2,
     };
 
-    
     try {
-      
-      await createUserService(newUser);
-      navigate("/login");
+      const createdUser = await createUserService(newUser);
+      navigate(`/create-account/${createdUser.data._id}`);
     } catch (error) {
-      console.log(error)
-      // vamos a determinar el tipo de error que recibimos, y actuar diferente
-      console.log(error.response.status); // codigo de error enviado
-      console.log(error.response.data.errorMessage); // el mensaje de error que dio el fallo
       if (error.response.status === 400) {
-        // mostramos al usuario como solventar el problema
         setErrorMessage(error.response.data.errorMessage);
       } else {
         navigate("/error");
       }
     }
   };
-  
-  console.log(loggedUser);
-  return (
 
-    
+  return (
     <div>
       {/* only show the render if is logged like admin */}
-      {isLoggedIn === false || isAdmin === false ? <Navigate to="/login" />:
+      {isLoggedIn === false || isAdmin === false ? (
+        <Navigate to="/login" />
+      ) : (
         <div>
           <h3>Creatre User</h3>
           <form onSubmit={handleSubmit}>
@@ -93,7 +85,6 @@ function CreateUserForm() {
             <input
               type="date"
               name="dob"
-              // value={dob}
               onChange={handleDobChange}
             />
             <br />
@@ -126,8 +117,7 @@ function CreateUserForm() {
             <button type="submit">Agregar</button>
           </form>
         </div>
-       }
-
+      )}
     </div>
   );
 }
