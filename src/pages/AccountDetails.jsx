054b01eb14b48d4c4bee6ef9287d6dd8cd8d2da2
DJGValls <jiga1980@gmail.com>
2023-03-15 10:24:42 +0100

    created credit card form view with css

--- src/pages/AccountDetails.jsx
+++ src/pages/AccountDetails.jsx
@@ -10,6 +10,9 @@ import {
   getSingleKittyAccountDetailsService,
 } from "../services/kitty.services";
 import { getAccountTransactionsService } from "../services/transfer.services";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Form } from "react-bootstrap";
 
 function AccountDetails() {
   const navigate = useNavigate();
@@ -19,6 +22,7 @@ function AccountDetails() {
   const [isFetching, setIsFetching] = useState(true);
   const { loggedUser } = useContext(AuthContext);
   const [errorMessage, setErrorMessage] = useState("");
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     getData();
@@ -38,7 +42,7 @@ function AccountDetails() {
       setTransactionList(transactions.data);
       setIsFetching(false);
     } catch (error) {
-      navigate("/error")
+      navigate("/error");
     }
   };
 
@@ -64,6 +68,9 @@ function AccountDetails() {
     return <h2>Spinner...</h2>;
   }
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
       <div>
@@ -73,7 +80,126 @@ function AccountDetails() {
         </p>
       </div>
       <div>
-        <button>Add Money</button>
+        <>
+          <Button variant="primary" onClick={handleShow}>
+            Add Money
+          </Button>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Body>
+              <div className="wrapper">
+                <div className="card px-4">
+                  <div className=" my-3">
+                    <p className="h8">Card number</p>
+                    <p className="text-muted ">
+                      Introduce your Credit Card credentials and the money to
+                      your virtual account
+                    </p>
+                  </div>
+
+                  <div className="debit-card mb-3">
+                    <div className="d-flex flex-column h-100">
+                      <label className="d-block">
+                        <div className="d-flex position-relative">
+                          <div>
+                            <img
+                              src="https://www.freepnglogos.com/uploads/visa-inc-logo-png-11.png"
+                              className="visa"
+                              alt=""
+                            ></img>
+                            <p className="mt-2 mb-4 text-white fw-bold"></p>
+                          </div>
+                          <div className="input">
+                            <input type="radio" name="card" id="check"></input>
+                          </div>
+                        </div>
+                      </label>
+                      <div className="mt-auto fw-bold d-flex align-items-center justify-content-between">
+                        <p>
+                          <input
+                            // minLength="16"
+                            // maxLength="16"
+                            className="card-Number-Form"
+                            size="16"
+                            type="text"
+                            placeholder="1234567890123456"
+                          />
+                        </p>
+
+                        <p>
+                          <input
+                            // minLength="5"
+                            // maxLength="5"
+                            className="card-Date-Form"
+                            size="5"
+                            type="text"
+                            placeholder="05/23"
+                          />
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="debit-card card-2 mb-4">
+                    <div className="d-flex flex-column h-100">
+                      <label className="d-block">
+                        <div className="d-flex position-relative">
+                          <div>
+                            <img
+                              src="https://www.freepnglogos.com/uploads/mastercard-png/mastercard-logo-png-transparent-svg-vector-bie-supply-0.png"
+                              alt="master"
+                              className="master"
+                            ></img>
+                          </div>
+                          <div className="input">
+                            <input type="radio" name="card" id="check"></input>
+                          </div>
+                        </div>
+                      </label>
+                      <div className="mt-auto fw-bold d-flex align-items-center justify-content-between">
+                        <p>
+                          <input
+                            // minlength="16"
+                            // maxlength="16"
+                            className="card-Number-Form"
+                            size="16"
+                            type="text"
+                            placeholder="1234567890123456"
+                          />
+                        </p>
+
+                        <p>
+                          <input
+                            // minLength="5"
+                            // maxLength="5"
+                            className="card-Date-Form"
+                            size="5"
+                            type="text"
+                            placeholder="05/23"
+                          />
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <Form.Control
+                    className="card-addMoney-form mb-4"
+                    size="lg"
+                    type="text"
+                    placeholder="0  €"
+                  />
+                  <div className="btnCard mb-4">Add Money</div>
+                  <div className="btnCard mb-4" onClick={handleClose}>
+                    Close
+                  </div>
+                </div>
+              </div>
+            </Modal.Body>
+          </Modal>
+        </>
       </div>
       <div>
         <h3>Movements</h3>
