9bdf8c9163203d0e0e6e993b18bb6fd6d2a2aeb3
Alvaro <alvarorivgar@gmail.com>
2023-03-16 10:27:40 +0100

    added icons and styles for navbar

--- src/pages/HomeAnon.jsx
+++ src/pages/HomeAnon.jsx
@@ -1,7 +1,67 @@
 function HomeAnon() {
   return (
-    <div>Welcome</div>
-  )
+    <div className="home-anon">
+      <img src="../images/logo.png" alt="logo" />
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+      <footer>
+        <div className="names">
+          <p>Álvaro Rivas</p>
+
+          <div className="links">
+            <a href="https://www.linkedin.com/in/%C3%A1lvaro-rivasg/">
+              <img
+                src="https://myclouddoor.com/wp-content/uploads/2019/11/Linkedin-logo.png"
+                alt="linkedin-icon"
+                width="50px"
+              />
+            </a>
+            <a href="https://github.com/alvarorivgar">
+              <img
+                src="https://pngimg.com/uploads/github/github_PNG40.png"
+                alt="git-icon"
+                width="40px"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="names">
+          <p>Dani Jiménez</p>
+          <div>
+            <a href="https://www.linkedin.com/in/daniel-jimenez-07092754/">
+              <img
+                src="https://myclouddoor.com/wp-content/uploads/2019/11/Linkedin-logo.png"
+                alt="linkedin-icon"
+                width="50px"
+              />
+            </a>
+            <a href="https://github.com/DJGValls">
+              <img
+                src="https://pngimg.com/uploads/github/github_PNG40.png"
+                alt="git-icon"
+                width="40px"
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default HomeAnon
+export default HomeAnon;
