581f989494fc2e303f9433a876901c443408f380
Alvaro <alvarorivgar@gmail.com>
2023-03-16 18:23:57 +0100

    added styles to home anon

--- src/pages/HomeAnon.jsx
+++ src/pages/HomeAnon.jsx
@@ -1,7 +1,43 @@
+import { Link } from "react-router-dom";
+
 function HomeAnon() {
   return (
     <div className="home-anon">
-      <img src="" alt="logo" />
+      <img
+        src="https://res.cloudinary.com/dkz1jslyi/image/upload/v1678960941/Kitten%20Kapital/logo-slogan_cxxpsa.png"
+        alt="logo"
+        id="logo-home"
+      />
+
+      <div id="home-body">
+        <div id="section-one">
+          <p>Kitten Kapital is the best option to keep your savings.</p>
+          <br />
+          <p>Try our Checking Account with very competitive fees.</p>
+          <br />
+          <p>You can always negotiate them with our trusted bank managers.</p>
+          <br />
+          <p>
+            If you are 25 or younger, you can open a Kitty Account completely
+            free, no fees whatsoever!
+          </p>
+          <br />
+        </div>
+        <div id="section-two">
+          <p>
+            You can become a client from the confort of your own home, <br />
+            you just
+            need a camera to contact one of our managers through a video call.
+          </p>
+          <img
+            src="https://res.cloudinary.com/dkz1jslyi/image/upload/v1678960941/Kitten%20Kapital/business-cat_osxh4v.png"
+            alt="cat"
+          />
+          <Link to="/video">
+            <button id="get-started">Get started!</button>
+          </Link>
+        </div>
+      </div>
 
       <footer>
         <div className="names">
